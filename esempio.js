af3b4cd29a5b346c612e8ddcaa192c4f1e31b464
DESKTOP-P7R7U05\michi <michipelle40@gmail.com>
2023-09-20 16:37:31 +0200

    final exercise

--- esempio.js
+++ esempio.js
@@ -1,1 +1,8 @@
 //primo esercizio in javascript
+
+//Write a node.js program to replace two or more a's with the letter b on the given string using Regular Expression.
+
+stringa = "saalve"
+
+console.log(stringa.replace(new RegExp("[Aa]{2,}"), "Ciao"));
+
